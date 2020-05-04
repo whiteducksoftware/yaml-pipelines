a080f6f04da42dbc4e9add36b396d937ed7fc96b
André Ratzenberger <andre.ratzenberger@whiteduck.de>
2020-05-04 16:26:33 +0200

    init

---
+++ src/yaml-demo-webapp/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using sample_mvc.Models;
+
+namespace sample_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Settings _settings;
+
+        public HomeController(IOptions<Settings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        public IActionResult Index()
+        {
+            var framework = Assembly
+                .GetEntryAssembly()?
+                .GetCustomAttribute<TargetFrameworkAttribute>()?
+                .FrameworkName;
+
+            ViewBag.Platform = new
+            {
+                OsVersion = System.Runtime.InteropServices.RuntimeInformation.OSDescription,
+                DotnetCoreVersion = framework
+            };
+
+            ViewBag.Title = _settings.Title;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/yaml-demo-webapp/README.md
@@ -0,0 +1,11 @@
+# Hi! I am Fred
+
+This is a simple ASP .NET Core 3.0 MVC Demo application that you can use to demonstrate e. g. containerization or deployments.
+
+You can test the latest version of this application [online](https://demo.mbrandl.com/).
+
+## Usage
+
+Just clone this repository and start the application using ```dotnet run```
+
+![Status](https://github.com/whiteducksoftware/sample-mvc/workflows/Deploy%20to%20Azure/badge.svg)
